d33202ed0999edf57fb0684ec4f76b63de1d5d59
vrobbin3247 <vaibhavgame3247@gmail.com>
2025-09-13 12:41:42 +0530

    implemented search tool

---
+++ Intro to agents/First_agent_template/app.py
@@ -0,0 +1,87 @@
+from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
+import datetime
+import requests
+import pytz
+import yaml
+from tools.final_answer import FinalAnswerTool
+
+from Gradio_UI import GradioUI
+import os
+
+# Below is an example of a tool that does nothing. Amaze us with your creativity !
+@tool
+def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
+    #Keep this format for the description / args / args description but feel free to modify the tool
+    """A tool that does nothing yet 
+    Args:
+        arg1: the first argument
+        arg2: the second argument
+    """
+    return "What magic will you build ?"
+
+
+web_tool = DuckDuckGoSearchTool(max_results=5)
+@tool
+def search_web(query:str)->str:
+    """A tool that fetches search results from DuckDuckGo for a given query.
+
+Args:
+    query: A string representing the search query or question (e.g., 'who discovered atoms').
+
+Returns:
+    A string containing the top search results from DuckDuckGo.
+"""
+    results = web_tool(query)
+    return results
+
+@tool
+def get_current_time_in_timezone(timezone: str) -> str:
+    """A tool that fetches the current local time in a specified timezone.
+    Args:
+        timezone: A string representing a valid timezone (e.g., 'America/New_York').
+    """
+    try:
+        # Create timezone object
+        tz = pytz.timezone(timezone)
+        # Get current time in that timezone
+        local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return f"The current local time in {timezone} is: {local_time}"
+    except Exception as e:
+        return f"Error fetching time for timezone '{timezone}': {str(e)}"
+
+
+final_answer = FinalAnswerTool()
+timezone_tool = get_current_time_in_timezone
+web_search_tool = search_web
+
+# If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
+# model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
+
+model = HfApiModel(
+max_tokens=2096,
+temperature=0.5,
+model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
+custom_role_conversions=None,
+)
+
+
+# Import tool from Hub
+image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+
+with open("prompts.yaml", 'r') as stream:
+    prompt_templates = yaml.safe_load(stream)
+    
+agent = CodeAgent(
+    model=model,
+    tools=[final_answer,timezone_tool,web_search_tool], ## add your tools here (don't remove final answer)
+    max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    prompt_templates=prompt_templates
+)
+
+
+GradioUI(agent).launch()
